ed93528671cde9e74f8c40ff8875ca84ff1b7409
ASUTOSH PASTOR <ashutoshpastor123@gmail.com>
2026-09-25 08:07:01 +0530

    Update CORS allowed origins for deployment

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,7 +11,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5500",
         "http://127.0.0.1:5500",
-        "https://eclectic-croquembouche-cbf721.netlify.app",
+        "https://singular-praline-081563.netlify.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
