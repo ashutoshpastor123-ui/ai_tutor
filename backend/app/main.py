c2496fe15cdd15b7adfdf5d7d6d2c507def3455e
ASUTOSH PASTOR <ashutoshpastor123@gmail.com>
2026-09-25 07:52:11 +0530

    Add Netlify app to CORS allowed origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,6 +11,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5500",
         "http://127.0.0.1:5500",
+        "https://eclectic-croquembouche-cbf721.netlify.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
